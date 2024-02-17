303e3511b3f920dada9687ee608e15059a3fcd99
Renjeeshrk <51906504+Renjeeshrk@users.noreply.github.com>
2024-02-17 12:13:30 +0530

    Created buildinfra.tf

---
+++ Terraform/buildinfra.tf
@@ -0,0 +1,31 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+# Crate Resource Group
+
+resource "azurerm_resource_group" "infra-rg01" {
+    name = "Production-RG-infra"
+    location = "eastus"
+}
+# Create Virtual network and subnet 
+resource "azurerm_virtual_network" "infra-network" {
+  name                = "Production-vNet-infra"
+  location            = azurerm_resource_group.infra-rg01.location
+  resource_group_name = azurerm_resource_group.infra-rg01.name
+  address_space       = ["192.168.0.0/16"]
+
+  subnet {
+    name           = "Production-Hub-infra"
+    address_prefix = "192.168.20.0/23"
+  }
+}
